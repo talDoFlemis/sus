611b8e1d4c0b9042dcb21f63e99940d4084bdeda
Said Rodrigues <coderflemis@gmail.com>
2024-11-30 18:25:40 -0300

    feat: make service spawn a reception

--- lib/service/include/service.h
+++ lib/service/include/service.h
@@ -2,15 +2,17 @@
 
 #define SERVICE_H
 
+#include "reception/include/reception.h"
 #include <unistd.h>
 
 typedef struct Service {
+  Reception *reception;
 } Service;
 
 // Creates a new service using a given reception and attendant
-Service *create_new_service();
+Service *create_new_service(Reception *reception);
 
 // Start a service in a different process
-pid_t start_service_process(Service *service);
+pid_t start_service_process(Service *self);
 
 #endif // !SERVICE_H
--- lib/service/src/service.c
+++ lib/service/src/service.c
@@ -1,18 +1,21 @@
 #include "service/include/service.h"
+#include "reception/include/reception.h"
 #include <assert.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdlib.h>
 
 const char *attendant_semaphore_path = "/sem_atend";
 const char *lng_semaphore_path = "/sem_block";
 
-Service *create_new_service() {
+Service *create_new_service(Reception *reception) {
   Service *service = malloc(sizeof(Service));
+  service->reception = reception;
   return service;
 };
 
-pid_t start_service_process(Service *service) {
+pid_t start_service_process(Service *self) {
   // Open attendant semaphore
   sem_t *sem_attendant =
       sem_open(attendant_semaphore_path, O_CREAT | O_EXCL, 0644, 1);
@@ -30,6 +33,11 @@ pid_t start_service_process(Service *service) {
 
   // Child process
   if (pid == 0) {
+    pthread_t reception_thread = spawn_reception_thread(self->reception);
+
+    int join_status = pthread_join(reception_thread, NULL);
+    assert(join_status != -1 && "failed to join reception thread on service");
+
     exit(EXIT_SUCCESS);
   }
   // Parent process
