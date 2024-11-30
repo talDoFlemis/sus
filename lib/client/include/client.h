d75427a47e2239427c1980f3add242728c9380e7
Said Rodrigues <coderflemis@gmail.com>
2024-11-30 18:24:45 -0300

    feat: add client process

--- lib/client/include/client.h
+++ lib/client/include/client.h
@@ -6,6 +6,23 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef enum ClientPriority {
+  High,
+  Standard,
+} ClientPriority;
+
+// Client Process Tuple
+typedef struct ClientProcess {
+  // Client PID for later SIGCONT
+  pid_t pid;
+  // Date of arrive of a client
+  struct timespec ts;
+  // Time to attend
+  int time_to_attend;
+  // Client priority
+  ClientPriority priority;
+} ClientProcess;
+
 void client(useconds_t sleep_time_in_us);
 
 #endif // !CLIENT_H
