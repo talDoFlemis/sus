a8ed7a176007de85b814ee85a6f1449b486addef
Said Rodrigues <coderflemis@gmail.com>
2024-12-01 20:15:32 -0300

    feat: add cleaner

---
+++ src/cleaner/main.c
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+  printf("Cleaning up semaphores...\n");
+  sem_unlink("/sem_atend");
+  sem_unlink("/sem_block");
+
+  printf("Cleaning up demand file...\n");
+  remove("demanda.txt");
+
+  return EXIT_SUCCESS;
+}
