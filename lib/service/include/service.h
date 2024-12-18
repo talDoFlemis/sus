51d87f0b39ca096a187a09cd3bd65d8facbd8d02
Said Rodrigues <coderflemis@gmail.com>
2024-12-17 21:48:21 -0300

    refactor: make service spawn attendant and join it thread

--- lib/service/include/service.h
+++ lib/service/include/service.h
@@ -3,14 +3,16 @@
 #define SERVICE_H
 
 #include "reception/include/reception.h"
+#include "attendant/include/attendant.h"
 #include <unistd.h>
 
 typedef struct Service {
   Reception *reception;
+  Attendant attendant;
 } Service;
 
 // Creates a new service using a given reception and attendant
-Service *create_new_service(Reception *reception);
+Service *create_new_service(Reception *reception, Attendant attendant);
 
 // Start a service in a different process
 pid_t start_service_process(Service *self);
